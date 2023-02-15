028d9b46ebe5784d1eee49538f5eaa41353b7852
pragathya <79365796+pragathya@users.noreply.github.com>
2023-02-15 13:48:41 +0530

    Create PES1UG20CS286.cpp

---
+++ PES1UG20CS286.cpp
@@ -0,0 +1,10 @@
+//File: PES1UG20CS286.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, This is Jenkins pipeline\n";
+  cout << "I have successfully built and run\n";
+  cout << "My name is Pragathya\n";
+  return 0;
+}
