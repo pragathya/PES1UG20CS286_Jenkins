3247f6b3462d94b44c4289523215474f050bc8c9
pragathya <79365796+pragathya@users.noreply.github.com>
2023-02-15 13:41:03 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "My name is Pragathya\n";
+  cout << "This indicates the code is modified\n";
   return 0;
 }
